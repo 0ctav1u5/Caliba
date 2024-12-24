60b2fed7eb53f313702b3195a7ed038392567590
jed12 <jed12@TITAN>
2024-12-24 18:11:14 +0000

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <limits>
+#include <thread>
+#include <Windows.h>
+#include <map>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include "MainHeader.hpp"
+
+
+int main(int argc, char* argv[]) {
+    
+
+
+
+
+
+
+
+    return 0;
+}
