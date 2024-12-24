9a5ae7b058f938577dac4af47e997686dd96a0d4
Darko Syndus <jed123e@gmail.com>
2024-12-24 19:14:36 +0000

    Basic SDL2 video initialisation and class setup

--- Main.cpp
+++ Main.cpp
@@ -14,8 +14,29 @@
 #include <SDL_ttf.h>
 #include "MainHeader.hpp"
 
+const int WINDOW_HEIGHT = 800; // global constants
+const int WINDOW_WIDTH = 800;
+const char* WINDOW_NAME = "Caliba";
 
-int main(int argc, char* argv[]) {
-    std::cout << "Testing Development Branch only." << std::endl;
+
+// we must first initialise the framework, then create the window and finally render the graphics
+// i will use main instead of SDL_Main for portability
+
+
+
+int main(int argc, char** argv[]) {
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) { 
+        std::cerr << "SDL could not be initialised! Error caught: " << SDL_GetError() << std::endl;
+        return -1;
+    } // checking to see if the SDL2 library can be initialised for processing video graphics
+
+
+
+
+
+
+
+
     return 0;
 }
--- MainHeader.hpp
+++ MainHeader.hpp
@@ -1,1 +1,27 @@
 #pragma once
+#include <iostream>
+
+
+
+
+class GameEngine {
+private:
+	
+
+
+
+
+
+
+};
+
+class Game : public GameEngine {
+private:
+
+
+
+
+
+
+
+};
